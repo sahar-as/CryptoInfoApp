648201f27d5fa8e72fbec7fd64b129c6f2b9452c
sahar-as <sahar.asadian90@gmail.com>
2023-02-05 10:00:50 +0330

    Initial commit

---
+++ app/src/main/java/ir/saharapps/cryptoinfoapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package ir.saharapps.cryptoinfoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ir.saharapps.cryptoinfoapp.ui.theme.CryptoInfoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptoInfoAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CryptoInfoAppTheme {
+        Greeting("Android")
+    }
+}
