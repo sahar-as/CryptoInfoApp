153947baf267083dbc65dd6f2feeeaef0df25ba8
sahar-as <sahar.asadian90@gmail.com>
2023-02-06 07:32:29 +0330

    first test just page 1 works properly

--- app/src/main/java/ir/saharapps/cryptoinfoapp/MainActivity.kt
+++ app/src/main/java/ir/saharapps/cryptoinfoapp/MainActivity.kt
@@ -3,6 +3,11 @@ package ir.saharapps.cryptoinfoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
 import ir.saharapps.cryptoinfoapp.presentaion.navigation.Navigation
 import ir.saharapps.cryptoinfoapp.presentaion.ui.theme.CryptoInfoAppTheme
@@ -16,6 +21,13 @@ class MainActivity : ComponentActivity() {
                 Navigation()
             }
         }
+    }
+}
+
+@Composable
+fun TestPage() {
+    Box(modifier = Modifier.fillMaxSize()){
+        Text(text = "Hello")
     }
 }
 
